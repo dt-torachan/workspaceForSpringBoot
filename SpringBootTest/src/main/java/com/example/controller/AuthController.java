cd4ba0dc76693f84542f5061749472a084dbf426
dt-torachan <55536520+dt-torachan@users.noreply.github.com>
2020-05-19 01:17:59 +0900

    add files

---
+++ SpringBootTest/src/main/java/com/example/controller/AuthController.java
@@ -0,0 +1,33 @@
+package com.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.dao.UserDAO;
+
+@Controller
+public class AuthController {
+
+	@RequestMapping(value = "/")
+	public String entry() {
+		return "entry";
+	}
+	
+	@RequestMapping(value = "/login")
+	public String login(@RequestParam("id") String id, @RequestParam("password") String password,Model model,UserDAO userDAO) {
+		
+		model = userDAO.searchById(id,password,model);
+		
+		if(model.getAttribute("errormessage") != null) {
+			return "entry";
+		}else if(!model.getAttribute("id").equals(id) || !model.getAttribute("password").equals(password)){
+			model.addAttribute("errormessage", "IDまたはパスワードが正しくありません");
+			return "entry";
+		}else {
+			return "login";
+		}
+	}
+	
+}
---
+++ SpringBootTest/src/main/java/com/example/dao/UserDAO.java
@@ -0,0 +1,41 @@
+package com.example.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Component
+public class UserDAO {
+	
+	@Transactional
+	public Model searchById(@RequestParam("id") String id,@RequestParam("password") String password,Model model) {
+		try {
+			Connection conn = DriverManager.getConnection("jdbc:postgres://ricbhkslwxzrqj:5cda005554442a939f9c68973f02ec0bc748280e632c11f0fdbaccfd8fcb9b9a@ec2-3-222-30-53.compute-1.amazonaws.com:5432/d3ebsimt1afacu","ricbhkslwxzrqj","5cda005554442a939f9c68973f02ec0bc748280e632c11f0fdbaccfd8fcb9b9a");
+			PreparedStatement pstmt = conn.prepareStatement("select * from users where id = ?");
+			pstmt.setString(1, id);
+			ResultSet rs = pstmt.executeQuery();
+			if(rs.next()) {
+				model.addAttribute("id", rs.getString("id"));
+				model.addAttribute("password", rs.getString("password"));
+				model.addAttribute("username", rs.getString("username"));
+				model.addAttribute("phone", rs.getString("phone"));
+				model.addAttribute("mail", rs.getString("mail"));
+				model.addAttribute("privilege", rs.getInt("privilege"));
+			}else {
+				model.addAttribute("errormessage", "登録されていないIDです");
+			}
+		}catch(SQLException e) {
+			model.addAttribute("errormessage", "ログインエラーが発生しました");
+			e.printStackTrace();
+		}
+			return model;
+	}
+
+}
